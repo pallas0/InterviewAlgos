062544401c3ef0e8cff5dd661896b32d0817b7c6
Amelia Risner <amelia.risner0@gmail.com>
2022-07-31 20:02:51 -0700

    solved

---
+++ charsinstring_object.js
@@ -0,0 +1,23 @@
+// counting the number of characters in string
+// Example: aaabbbbcccc
+// count('aaabbbbccccc') => 'a3b4c5'
+// count ('abc') => 'a1b1c1'
+
+function count(string) {
+    let stringObject = {}
+
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] in stringObject) {
+            stringObject[string[i]] += 1;
+        }
+        else {
+            stringObject[string[i]] = 1;
+        }
+    }
+
+    stringObject = JSON.stringify(stringObject).replace(/[^a-z0-9]/gi, '');
+
+    return stringObject;
+}
+
+console.log(count('aabbbcccaa'))
