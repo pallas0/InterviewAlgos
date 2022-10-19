ebc2a25acc22c5249840ee470ac245a4d64085a5
Amelia Risner <amelia.risner0@gmail.com>
2022-10-18 17:05:06 -0700

    amended, got that time complexity way the hell down

--- birthday.js
+++ birthday.js
@@ -1,14 +1,22 @@
 //https://www.hackerrank.com/challenges/one-month-preparation-kit-the-birthday-bar/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
 
 function birthday(s, d, m) {
-    console.log(s, d, m)
     let counter = 0;
-    let temp = [];
-    for (let i =0; i <= s.length - m; i++) {
-        temp = s.slice(i, i+m).reduce((a,b) => a+b, 0);
-        if (temp == d) {
-            counter += 1;
+    let sum = 0;
+    
+    for (let i = 0; i < m; i++) {
+        sum += s[i];
+    }
+    
+    if (sum == d) {
+        counter++;
+    }
+    for (let i=m; i < s.length; i++) {
+        sum += s[i] - s[i - m];
+        if (sum == d) {
+            counter++;
         }
     }
+    
     return counter;
 }
